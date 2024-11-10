4a26f2f4fd61ae55c405d811cc61e579c00e09e1
Mahdi Alikhani2005 <mahdialikhaniapr2005@gmail.com>
2024-11-10 16:25:35 +0330

    Navbar&&Header/done

--- Todo-App/src/App.jsx
+++ Todo-App/src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
 import Navbar from "./Components/Navbar";
+// import Header from "./Components/Header";
 function App() {
-  return <Navbar />;
+  return (
+    <>
+      {/* <Header /> */}
+      <Navbar />;
+    </>
+  );
 }
 
 export default App;
--- Todo-App/src/Components/Navbar.jsx
+++ Todo-App/src/Components/Navbar.jsx
@@ -1,88 +1,216 @@
+import React from "react";
+import Header from "./Header";
+import {
+  IconButton,
+  Typography,
+  List,
+  ListItem,
+  ListItemPrefix,
+  ListItemSuffix,
+  Chip,
+  Accordion,
+  AccordionHeader,
+  AccordionBody,
+  Drawer,
+  Card,
+  Button,
+} from "@material-tailwind/react";
+import {
+  PresentationChartBarIcon,
+  ShoppingBagIcon,
+  UserCircleIcon,
+  Cog6ToothIcon,
+  InboxIcon,
+  PowerIcon,
+} from "@heroicons/react/24/solid";
+import {
+  ChevronRightIcon,
+  ChevronDownIcon,
+  Bars3Icon,
+  XMarkIcon,
+} from "@heroicons/react/24/outline";
+
 export default function Navbar() {
+  const [open, setOpen] = React.useState(0);
+
+  const [isDrawerOpen, setIsDrawerOpen] = React.useState(false);
+
+  const handleOpen = (value) => {
+    setOpen(open === value ? 0 : value);
+  };
+
+  const openDrawer = () => setIsDrawerOpen(true);
+  const closeDrawer = () => setIsDrawerOpen(false);
+
   return (
     <>
-      <div className="relative flex flex-col bg-clip-border rounded-xl bg-white text-gray-700 h-[calc(100vh-2rem)] w-full max-w-[20rem] p-4 shadow-xl shadow-blue-gray-900/5">
-        <div className="mb-2 p-4">
-          <h5 className="block antialiased tracking-normal font-sans text-xl font-semibold leading-snug text-gray-900">
-            Material Tailwind
-          </h5>
-        </div>
-        <nav className="flex flex-col gap-1 min-w-[240px] p-2 font-sans text-base font-normal text-gray-700">
-          <div
-            role="button"
-            className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-gray-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
+      <div className="flex">
+        {/* Sidebar for larger screens */}
+        <div
+          className="hidden lg:flex lg:flex-col lg:w-64 lg:bg-gray-100 lg:h-screen"
+          style={{ background: " #e8eaf6" }}
+        >
+          <Card
+            color="transparent"
+            shadow={false}
+            className="h-[calc(100vh-2rem)] w-full p-4"
           >
-            <div className="grid place-items-center mr-4"></div>
-            Blocks
-          </div>
-          <div
-            role="button"
-            className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
-          >
-            <div className="grid place-items-center mr-4"></div>
-            Books
-          </div>
-          <div
-            role="button"
-            className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
-          >
-            <div className="grid place-items-center mr-4"></div>
-            Example Pages{" "}
-            <div className="grid place-items-center ml-auto justify-self-end">
-              <div
-                className="relative grid items-center font-sans font-bold uppercase whitespace-nowrap select-none bg-blue-500/20 text-blue-900 py-1 px-2 text-xs rounded-full"
-                style="opacity: 1;"
-              >
-                <span className="">14</span>
-              </div>
+            <div className="mb-2 flex items-center gap-4 p-3">
+              <Typography variant="h5" color="blue-gray">
+                To-Do List
+              </Typography>
             </div>
-          </div>
-          <div
-            role="button"
-            className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
-          >
-            <div className="grid place-items-center mr-4"></div>
-            Profile
-          </div>
-          <div
-            role="button"
-            className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
-          >
-            <div className="grid place-items-center mr-4"></div>
-            Settings
-          </div>
-          <div
-            role="button"
-            className="flex items-center w-full p-3 rounded-lg text-start leading-tight transition-all hover:bg-blue-50 hover:bg-opacity-80 focus:bg-blue-50 focus:bg-opacity-80 active:bg-blue-50 active:bg-opacity-80 hover:text-blue-900 focus:text-blue-900 active:text-blue-900 outline-none"
-          >
-            <div className="grid place-items-center mr-4"></div>
-            Log Out
-          </div>
-        </nav>
-      </div>
-
-      <div className="w-full pt-5 px-4 mb-8 mx-auto ">
-        <div className="text-sm text-gray-700 py-1">
-          Made with{" "}
-          <a
-            className="text-gray-700 font-semibold"
-            href="https://www.material-tailwind.com/docs/react/sidebar?ref=tailwindcomponents"
-            target="_blank"
-          >
-            Material Tailwind
-          </a>{" "}
-          by{" "}
-          <a
-            href="https://www.creative-tim.com?ref=tailwindcomponents"
-            className="text-gray-700 font-semibold"
-            target="_blank"
-          >
-            {" "}
-            Creative Tim
-          </a>
-          .
+            <div className="p-2">
+              <Button style={{ background: "#0288d1" }} fullWidth>
+                block level button
+              </Button>
+            </div>
+            <List>
+              <Accordion
+                open={open === 1}
+                icon={
+                  <ChevronDownIcon
+                    strokeWidth={2.5}
+                    className={`mx-auto h-4 w-4 transition-transform ${
+                      open === 1 ? "rotate-180" : ""
+                    }`}
+                  />
+                }
+              >
+                <ListItem className="p-0" selected={open === 1}>
+                  <AccordionHeader
+                    onClick={() => handleOpen(1)}
+                    className="border-b-0 p-3"
+                  >
+                    <ListItemPrefix>
+                      <PresentationChartBarIcon className="h-5 w-5" />
+                    </ListItemPrefix>
+                    <Typography
+                      color="blue-gray"
+                      className="mr-auto font-normal"
+                    >
+                      Dashboard
+                    </Typography>
+                  </AccordionHeader>
+                </ListItem>
+                <AccordionBody className="py-1">
+                  <List className="p-0">
+                    <ListItem>
+                      <ListItemPrefix>
+                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                      </ListItemPrefix>
+                      Analytics
+                    </ListItem>
+                    <ListItem>
+                      <ListItemPrefix>
+                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                      </ListItemPrefix>
+                      Reporting
+                    </ListItem>
+                    <ListItem>
+                      <ListItemPrefix>
+                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                      </ListItemPrefix>
+                      Projects
+                    </ListItem>
+                  </List>
+                </AccordionBody>
+              </Accordion>
+              <Accordion
+                open={open === 2}
+                icon={
+                  <ChevronDownIcon
+                    strokeWidth={2.5}
+                    className={`mx-auto h-4 w-4 transition-transform ${
+                      open === 2 ? "rotate-180" : ""
+                    }`}
+                  />
+                }
+              >
+                <ListItem className="p-0" selected={open === 2}>
+                  <AccordionHeader
+                    onClick={() => handleOpen(2)}
+                    className="border-b-0 p-3"
+                  >
+                    <ListItemPrefix>
+                      <ShoppingBagIcon className="h-5 w-5" />
+                    </ListItemPrefix>
+                    <Typography
+                      color="blue-gray"
+                      className="mr-auto font-normal"
+                    >
+                      E-Commerce
+                    </Typography>
+                  </AccordionHeader>
+                </ListItem>
+                <AccordionBody className="py-1">
+                  <List className="p-0">
+                    <ListItem>
+                      <ListItemPrefix>
+                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                      </ListItemPrefix>
+                      Orders
+                    </ListItem>
+                    <ListItem>
+                      <ListItemPrefix>
+                        <ChevronRightIcon strokeWidth={3} className="h-3 w-5" />
+                      </ListItemPrefix>
+                      Products
+                    </ListItem>
+                  </List>
+                </AccordionBody>
+              </Accordion>
+              <hr className="my-2 border-blue-gray-50" />
+              <ListItem>
+                <ListItemPrefix>
+                  <InboxIcon className="h-5 w-5" />
+                </ListItemPrefix>
+                Inbox
+                <ListItemSuffix>
+                  <Chip
+                    value="14"
+                    size="sm"
+                    variant="ghost"
+                    color="blue-gray"
+                    className="rounded-full"
+                  />
+                </ListItemSuffix>
+              </ListItem>
+              <ListItem>
+                <ListItemPrefix>
+                  <UserCircleIcon className="h-5 w-5" />
+                </ListItemPrefix>
+                Profile
+              </ListItem>
+              <ListItem>
+                <ListItemPrefix>
+                  <Cog6ToothIcon className="h-5 w-5" />
+                </ListItemPrefix>
+                Settings
+              </ListItem>
+              <ListItem>
+                <ListItemPrefix>
+                  <PowerIcon className="h-5 w-5" />
+                </ListItemPrefix>
+                Log Out
+              </ListItem>
+            </List>
+          </Card>
         </div>
+        <Header />
       </div>
     </>
   );
+}
+
+{
+  /* <IconButton variant="text" size="lg" onClick={openDrawer}>
+{isDrawerOpen ? (
+  <XMarkIcon className="h-8 w-8 stroke-2" />
+) : (
+  <Bars3Icon className="h-8 w-8 stroke-2" />
+)}
+</IconButton>
+<Drawer open={isDrawerOpen} onClose={closeDrawer}>
+</Drawer> */
 }
